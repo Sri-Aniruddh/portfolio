6619f1954a3707db2af73aa9231c98d763e9e7bd
Aniruddh Srivastava <137644646+Sri-Aniruddh@users.noreply.github.com>
2024-01-21 23:58:24 +0530

    Update Container.jsx

--- src/component/container/Container.jsx
+++ src/component/container/Container.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
-export default function Container({children}) {
-    
+export default function Container({ children }) {
 
-    return <div className='w-full flex relative max-w-7xl mx-0 px-0  dark:bg-gray-900  dark:border-black'>{children}</div>;
+
+    return (
+        <div className="container-fluid shadow-sm mx-auto  dark:bg-gray-900  dark:border-black">
+            {children}
+        </div>
+
+
+
+    )
 }
