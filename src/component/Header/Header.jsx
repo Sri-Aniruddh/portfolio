ef9324c7e26f570e3d21f48226ca6b5382d24237
Aniruddh Srivastava <137644646+Sri-Aniruddh@users.noreply.github.com>
2024-01-21 23:58:28 +0530

    Update Header.jsx

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -35,43 +35,35 @@ function Header() {
 
 
   return (
-    <div className="w-full flex m-0 dark:bg-gray-900 dark:text-white mb-0">
-
-      <header className="py-2 m-0 w-full border   shadow">
-        <Container >
-          <nav className='flex w-full relative dark:bg-gray-800'>
-            <div className='mr-4 col-2 flex items-end mx-0'>
+    <Container>
+        <header className="py-2 w-auto flex m-0 dark:bg-gray-900 dark:text-white mb-0 ">
+          <nav className=" p-4">
+            <div className="flex mx-auto mr-4 items-end ">
               <Link to='/'>
                 <Logo width='70px' />
 
               </Link><h4 className='item-center mb-4 text-yellow-300 font-serif font-bold'>Portfolio</h4>
-            </div>
-            <div className='col-10 flex'>
-              <ul className='flex ml-auto m-6 '>
-                {navItems.map((item) =>
-                  item.active ? (
-                    <li key={item.name}>
-                      <button
-                        onClick={() => navigate(item.slug)}
-                        className='inline-bock px-6 py-2 duration-200 hover:bg-yellow-400 rounded-full'
-                      >{item.name}</button>
-                    </li>
-                  ) : null
-                )}
-                {/* {authStatus && (
-                    <li>
-                      <LogoutBtn />
-                    </li>
-                  )} */}
 
-                <ThemeBtn />
+                <ul className='ml-auto d-flex flex-row justify-content-end'>
+                  {navItems.map((item) =>
+                    item.active ? (
+                      <li key={item.name}>
+                        <button
+                          onClick={() => navigate(item.slug)}
+                          className='inline-bock px-6 py-2 duration-200 hover:bg-yellow-400 rounded-full'
+                        >{item.name}</button>
+                      </li>
+                    ) : null
+                  )}
+                  <ThemeBtn />
 
-              </ul>
+                </ul>
+                
             </div>
           </nav>
-        </Container>
-      </header>
-    </div>
+
+        </header>
+    </Container>
   )
 }
 
